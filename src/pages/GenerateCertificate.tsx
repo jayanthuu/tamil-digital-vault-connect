4cb5b04bb7d407784a4cb99f5c6b4cd1168af93d
Jayanthi R <152688800+jayanthuu@users.noreply.github.com>
2025-06-15 20:38:48 +0530

    Create GenerateCertificate.tsx
    added GenerateCertificate.tsx

---
+++ src/pages/GenerateCertificate.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CertificateForm from "../components/CertificateForm";
+
+const GenerateCertificate: React.FC = () => {
+  return (
+    <div>
+      <h1>Generate Certificate</h1>
+      <CertificateForm />
+    </div>
+  );
+};
+
+export default GenerateCertificate;
